3daff32e32a4c9717066e44ad5cae368a26e6ee5
Ppamo <pablo@ppamo.cl>
2017-11-07 02:05:15 -0300

    rewrite pdf generation script

--- res/build/build.sh
+++ res/build/build.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
+TMP=.tmp
 
-doit(){
-	OUTPUT=$1
-	INPUT=$2
-	rm -f $OUTPUT
-	gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=$OUTPUT $INPUT
-	chown pablo.pablo $OUTPUT
+generate_pdf_pages(){
+	rm -Rf $TMP
+	mkdir -p $TMP
+	for i in $*
+	do
+		if [ -f $i ]
+		then
+			FILENAME=$(basename $i .svg)
+			echo "   converting $FILENAME..."
+			inkscape -f $i --export-pdf=$TMP/$FILENAME.pdf
+		fi
+	done
 }
 
-doit ../Pablo.Mansilla.Ojeda.cv.es.pdf "es/page*.pdf"
-doit ../Pablo.Mansilla.Ojeda.cv.en.pdf "en/page*.pdf"
+join_pdf_pages(){
+	OUTPUT=$1
+	rm -f $OUTPUT
+	echo "=> generating $OUTPUT"
+	gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=$OUTPUT $TMP/*
+	chown pablo.pablo $OUTPUT
+	rm -Rf $TMP
+	echo
+}
+
+generate_pdf_pages ../en/*
+join_pdf_pages ../Pablo.Mansilla.Ojeda.cv.en.pdf
+
+generate_pdf_pages ../es/*
+join_pdf_pages ../Pablo.Mansilla.Ojeda.cv.es.pdf
